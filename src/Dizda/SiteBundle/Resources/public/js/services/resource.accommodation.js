ecb5b2cac5de061add12a9eaa99dccfb5e2f408b
Jonathan Dizdarevic <dizzda@gmail.com>
2013-10-03 23:48:26 +0200

    fixed comment issue

--- src/Dizda/SiteBundle/Resources/public/js/services/resource.accommodation.js
+++ src/Dizda/SiteBundle/Resources/public/js/services/resource.accommodation.js
@@ -4,7 +4,7 @@ factory('Accommodation', function($resource){
         query:    {method:'GET',    params: {id:'@id'}, isArray:true},
         favorite: {method:'PATCH',  params: {id:'@id',  operation:'favorite'}},
         viewed:   {method:'PATCH',  params: {id:'@id',  operation:'viewed'}},
-        comment:  {method:'POST',   params: {id:'@id',  operation:'comment'}, isArray:true},
+        comment:  {method:'POST',   params: {id:'@id',  operation:'comments'}, isArray:true},
         delete:   {method:'DELETE', params: {id:'@id'}},
         versions: {method:'GET',    params: {id:'@id',  operation:'versions'}, isArray:true}
     });
